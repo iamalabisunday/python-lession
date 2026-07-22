0b1de6249ddc8ac5478c823f43acb71afaaf8959
Sunday Alabi <iamalabisunday@gmail.com>
2026-07-22 23:55:17 +0100

    initial commit

--- question_36.py
+++ question_36.py
@@ -1,1 +1,36 @@
 # Write a Python Program to check if given array is Monotonic.
+def check_if_array_monotonic(num):
+    if num < 1:
+        return "Error: enter a vaild positive number"
+    
+    arrays = []
+    for _ in range(num):
+        array = int(input("Enter number: ")) 
+        arrays.append(array)
+        
+    increasing = decreasing = True
+    
+    for i in range(1, len(arrays)):
+        if arrays[i] > arrays[i-1]:
+            decreasing = False
+        elif  arrays[i] < arrays[i-1]:
+            increasing = False
+        
+    return arrays, (increasing or decreasing)
+
+while True:
+    try: 
+        num = int(input("Enter number of element in an array: "))
+        arrays, result = check_if_array_monotonic(num)
+        print("---------------Ans:------------------------")
+        print(f"{arrays} is monotonic: {result}")
+        print("-------------------------------------------")
+
+    except ValueError:
+        print("-------------------------------------------")
+        print("Error")
+        print("-------------------------------------------")
+        
+    again = input("Do you want to try again - y/n: ").lower()
+    if again != "y":
+        break
---
+++ question_37.py
@@ -0,0 +1,51 @@
+# Write a Python Program to Add Two Matrices.
+
+def matrices(rows, cols):
+    if rows < 1 or cols < 1:
+        return []
+    
+    matrix = []
+    for i in range(rows):
+        rows = []
+        for j in range(cols):
+            val = int(input(f"Enter value at ({i},{j}): "))
+            rows.append(val)
+        matrix.append(rows)
+        
+    return matrix
+
+def add_matrices(matrix_1, matrix_2):
+    result = []
+    for i in range(len(matrix_1)):
+        row = []
+        for j in range(len(matrix_1[0])):
+            row.append(matrix_1[i][j] + matrix_2[i][j])
+        result.append(row)
+    return result
+        
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+    
+while True:
+    try:
+        row = int(input("Enter the number of rows: "))
+        col = int(input("Enter the number of columns: "))
+
+        print("Enter values for Matrix 1:")
+        matrix_1 = matrices(row, col)
+
+        print("Enter values for Matrix 2:")
+        matrix_2 = matrices(row, col)
+
+        result = add_matrices(matrix_1, matrix_2)
+
+        print("Result of addition:")
+        print_matrix(result)
+        
+    except ValueError:
+        print("Error")
+        
+    again = input("Do you want to try again - y/n: ").lower().strip()
+    if again != "y":
+        break
